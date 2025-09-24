40a5e849af057e982e0ddc8aeea0b2b9fa823ff3
rzak-shiatu <r.zakupero@gmail.com>
2025-09-25 00:57:27 +0900

    Update SprintersStakes.py
    Change

--- SprintersStakes.py
+++ SprintersStakes.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
 # フォント設定（全体に適用）
-rcParams['font.family'] = 'Meiryo'  # Windowsならメイリオ
+plt.rcParams["font.family"] = ["Noto Sans CJK JP", "sans-serif"]
+# rcParams['font.family'] = 'Meiryo'  # Windowsならメイリオ
 # macOS: 'Hiragino Sans'
 # Linux: 'IPAexGothic' など
 
@@ -251,3 +252,4 @@ elif mode == "年ごとの平均馬体重・平均上がり3F":
     plt.title("スプリンターズS 過去10年 平均馬体重・平均上がり3Fの推移")
 
     st.pyplot(fig)
+
